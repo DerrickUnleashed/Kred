5cde5d40c81ba3467ad84aecf5a530b6c0e7b8c2
DerrickUnleashed <derricksamuel.r@gmail.com>
2026-03-28 11:36:49 +0530

    Adding Spline Bot

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -5,12 +5,14 @@ import HowItWorks from './HowItWorks';
 import Pricing from './Pricing';
 import CTA from './CTA';
 import Footer from './Footer';
+import SplineBot from './SplineBot';
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen bg-background overflow-x-hidden">
       <Navbar />
       <Hero />
+      <SplineBot />
       <Features />
       <HowItWorks />
       <CTA />
---
+++ frontend/src/components/SplineBot.jsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/D6VPvasrYt5mQ-bf/scene.splinecode" 
+      />
+    </main>
+  );
+}
