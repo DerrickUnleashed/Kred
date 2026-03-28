34e581272ab9641fee14e972a21980b1eb89e38b
DerrickUnleashed <derricksamuel.r@gmail.com>
2026-03-28 22:39:50 +0530

    Removing redundant files

--- backend/budget_system/models/user.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, Float
-from budget_system.db.database import Base
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    monthly_income = Column(Float)
-    daily_limit = Column(Float)
--- backend/budget_system/services/budget.py
+++
@@ -1,4 +0,0 @@
-def generate_daily_limit(income: float):
-    savings_ratio = 0.2
-    usable_income = income * (1 - savings_ratio)
-    return round(usable_income / 30, 2)
