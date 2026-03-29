3f1945a31f52d2940f76595d297a77a7530c125b
DerrickUnleashed <derricksamuel.r@gmail.com>
2026-03-29 12:12:09 +0530

    Add Kred Bird Component

---
+++ frontend/src/components/KredBird.jsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/bUDuHGHrqOSQKxWh/scene.splinecode" 
+      />
+    </main>
+  );
+}
